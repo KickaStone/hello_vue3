e1e9b972d13e9d5e4fc57dad4b66a915e84f593f
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-14 21:36:54 +0800

    P4 Rewrite src folder. A simple App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export default {
+    name: 'App' // Component name
+  }
+</script>
+
+<template>
+  <div>
+    <h1 class="app">
+      Hello World !
+    </h1>
+  </div>
+  
+</template>
+
+<style scoped>
+  .app{
+    background-color: lightgray;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+
+</style>
