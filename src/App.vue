c217fa044bc184a355afbd20a78900cebacba38f
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-16 21:04:55 +0800

    P22: ref

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script lang="ts">
+<script lang="ts" setup name="App">
   import Person from "./components/Person.vue";
-  export default {
-    name: 'App', // Component name,
-    components:{Person}
+  import {ref} from "vue";
+
+  let title = ref()
+  let person = ref()
+
+  function showLog(){
+    console.log(title.value)
+  }
+
+  function showPerson(){
+    console.log(person.value)
   }
 </script>
 <template>
-  <Person/>
-<!--  <Car/>-->
+  <h2 ref="title">title</h2>
+  <button @click="showLog">showTitleByRef</button>
+  <Person ref='person'/>
+  <button @click="showPerson">showPerson</button>
 </template>
 
 <style scoped>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,42 +1,27 @@
 <template>
   <div class="person">
-    <h2>Temperature: {{temp}}</h2>
-    <h2>Height: {{height}}</h2>
-    <button @click="changeTemp">changeTemp</button>
-    <button @click="changeHeight">changeHeight</button>
+    <h2 ref="title">title1</h2>
+    <button @click="showLog">showTitle1ByRef</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import {ref, watch, watchEffect} from "vue";
+  import {ref} from "vue";
 
-  let temp = ref(0);
-  let height = ref(0);
+  let a = ref(0);
+  let b = ref(1);
+  let c = ref(2);
 
-  function changeTemp() {
-    temp.value  += 10;
+  let title = ref()
+  let person = ref()
+  function showLog(){
+    console.log(title.value);
   }
 
-  function changeHeight() {
-    height.value += 10;
-  }
-
-  // watch([temp, height], (value)=>{
-  //   // get latest temperature and height
-  //   let [newTemp, newHeight] = value;
-  //   console.log(newTemp, newHeight);
-  //   if(newTemp >= 60 || newHeight >= 80){
-  //     console.log('Send a request to server....');
-  //   }
-  // })
-  watchEffect(() => {
-    if(temp.value > 60 || height.value > 80){
-      console.log('Send a request to server....')
-    }
-  })
+  defineExpose({a, b});
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
 .person{
   background-color: lightblue;
   box-shadow: 0 0 10px;
