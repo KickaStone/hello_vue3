a26ecc89266ffde159a4db6a9a5d1235f5657fad
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-16 20:11:34 +0800

    P20:  watch_4, watch the attribute of a responsive object

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,34 +1,63 @@
 <template>
   <div class="person">
-    <h2>{{person.name}}</h2>
-    <h2>{{person.age}}</h2>
+    <h2>Name: {{person.name}}</h2>
+    <h2>Age: {{person.age}}</h2>
+    <h2>Car: {{ person.car.c1}}, {{ person.car.c2 }}</h2>
+    <button @click="changeName">changeName</button>
+    <button @click="changeAge">changeAge</button>
+    <button @click="changeC1">changeC1</button>
+    <button @click="changeC2">changeC2</button>
+    <button @click="changeCar">changeCar</button>
 
-  <button @click="changeName">Change Name</button>
-  <button @click="changeAge">Change Age</button>
-  <button @click="changePerson">Change Person</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {reactive, watch} from 'vue';
 
-let person = reactive({name: 'bob', age:18});
+import {reactive, watch} from "vue";
+
+let person = reactive({
+  name: "BOB",
+  age: 18,
+  car: {
+    c1: 'Benz',
+    c2: 'BWM',
+  }
+})
 
 function changeName() {
-  person.name  = 'Alice';
+  person.name += '~';
 }
-
 function changeAge() {
   person.age += 1;
 }
-
-function changePerson() {
-  Object.assign(person, {name:"Mark", age: 18});
+function changeCar() {
+  person.car = {
+    c1: 'Benz2024',
+    c2: 'BWM2024',
+  }
+}
+function changeC1(){
+  person.car.c1 = 'JEEP';
 }
 
-watch(person, (newValue) => {
-  console.log(newValue);
-}) // watch reactive object, default { deep: true }, and cannot disable
+function changeC2(){
+  person.car.c2 = 'Audi';
+}
+
+// watch(person.name, (newValue)=>{
+//   console.log(person, newValue);
+// });
+
+// watch a basic variable
+// watch(() => person.name, (newValue) => { // watch a getter function
+//   console.log(newValue);
+// })
+
+watch(() => person.car, (newValue) => {  // best practice
+  console.log(newValue)
+}, {deep: true})
+
 
 </script>
 
