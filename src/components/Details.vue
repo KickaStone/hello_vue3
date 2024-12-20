f0050c8b3f9c737e1d4b0cd59e423942a28f049a
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-20 22:45:21 +0800

    P36: Routing - Query

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-
+<script setup lang="ts" name="Details">
+  import {useRoute} from "vue-router";
+  import {toRefs} from "vue";
+  const route = useRoute();
+  console.log(route);
+  let {query} = toRefs(route);
 </script>
 
 <template>
-  Details
+  <li>ID: {{ query.id }}</li>
+  <li>Title: {{ query.title }}</li>
+  <li>Content: {{ query.content }}</li>
 </template>
 
 <style scoped>
--- src/components/News.vue
+++ src/components/News.vue
@@ -3,9 +3,9 @@
 
   const newsList = reactive([
     {id: 'news1', title: "Breaking: Major Event Unfolds", content: "In a surprising turn of events, details are emerging about a significant incident that has caught the public's attention."},
-    {id: 'news1', title: "BLocal Hero Saves the Day", content: "A local resident has been praised for their bravery after intervening in a crisis situation."},
-    {id: 'news1', title: "New Discoveries in Science", content: "Researchers have made groundbreaking discoveries that could change our understanding of the universe."},
-    {id: 'news1', title: "Sports Team Wins Championship", content: "In an exciting finale, the local sports team clinched the championship title against all odds."},
+    {id: 'news2', title: "BLocal Hero Saves the Day", content: "A local resident has been praised for their bravery after intervening in a crisis situation."},
+    {id: 'news3', title: "New Discoveries in Science", content: "Researchers have made groundbreaking discoveries that could change our understanding of the universe."},
+    {id: 'news4', title: "Sports Team Wins Championship", content: "In an exciting finale, the local sports team clinched the championship title against all odds."},
   ]);
 
 </script>
@@ -15,7 +15,19 @@
 <!--    display news titles -->
     <ul>
       <li v-for="news in newsList" :key="news.id">
-        <RouterLink to="/news/details"> {{news.title}}</RouterLink>
+<!--    first way-->
+<!--        <RouterLink :to="`/news/details?id=${news.id}&title=${news.title}&content=${news.content}`"> {{news.title}}</RouterLink>-->
+<!--    second way -->
+        <RouterLink :to="{
+          path: '/news/details/',
+          query: {
+            id: news.id,
+            title: news.title,
+            content: news.content
+          }
+        }">
+          {{news.title}}
+        </RouterLink>
       </li>
     </ul>
 <!--    display news content -->
