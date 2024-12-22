d2ca45a4da6760f0397b60caa9ba98201cbc4a91
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-22 12:58:25 +0800

    P51: Composition API

--- src/store/talk.ts
+++ src/store/talk.ts
@@ -1,20 +1,38 @@
 import {defineStore} from "pinia";
 import axios from "axios";
 import {nanoid} from "nanoid";
+import {reactive} from "vue";
 
-export const useTalkStore = defineStore("talk", {
-    state() {
-        return {
-            wordList: JSON.parse(localStorage.getItem('talkList') as string) || []
-        }
-    },
-    actions: {
-        async getATalk()
-        {
-            let {data: {content}} = await axios.get("https://api.uomg.com/api/rand.qinghua?format=json");
-            let obj = {id: nanoid(), title: content};
-            this.wordList.unshift(obj);
-        }
+// Option API
+// export const useTalkStore = defineStore("talk", {
+//     state() {
+//         return {
+//             wordList: JSON.parse(localStorage.getItem('talkList') as string) || []
+//         }
+//     },
+//     actions: {
+//         async getATalk()
+//         {
+//             let {data: {content}} = await axios.get("https://api.uomg.com/api/rand.qinghua?format=json");
+//             let obj = {id: nanoid(), title: content};
+//             this.wordList.unshift(obj);
+//         }
+//
+//     }
+// })
 
+// Composition API
+export const useTalkStore = defineStore("talk", ()=>{
+    const wordList = reactive(JSON.parse(localStorage.getItem('talkList') as string) || [])
+
+    async function getATalk()
+    {
+        let {data: {content}} = await axios.get("https://api.uomg.com/api/rand.qinghua?format=json");
+        let obj = {id: nanoid(), title: content};
+        wordList.unshift(obj);
     }
+
+    return {wordList, getATalk};
 })
+
+
